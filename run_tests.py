6690c0082157fb881cf17d72edfa4147049bbab5
saerx <saerlaith.robyn@gmail.com>
2020-11-28 14:06:51 +0000

    tested tag model

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,7 @@
 import unittest
  
 from tests.merchant_test import TestMerchant
+from tests.tag_test import TestTag
  
  
 if __name__ == '__main__':
---
+++ tests/tag_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from models.tag import Tag
+
+class TestTag(unittest.TestCase):
+
+    def setUp(self):
+        self.tag = Tag("Groceries", 2)
+
+    def test_tag_has_name(self):
+        self.assertEqual = (self.tag.name, "Groceries")
+
+    def test_tag_has_id(self):
+        self.assertEqual = (self.tag.id, 2)
