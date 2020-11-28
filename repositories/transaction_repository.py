76b70d92e23b218d2f526e7c6f2dd10f3b4d206c
saerx <saerlaith.robyn@gmail.com>
2020-11-28 19:54:07 +0000

    filled in crud for transaction repository

---
+++ repositories/transaction_repository.py
@@ -0,0 +1,61 @@
+from db.run_sql import run_sql
+
+from models.transaction import Transaction
+
+from models.tag import Tag
+import repositories.tag_repository as tag_repository
+
+from models.merchant import Merchant
+import repositories.merchant_repository as merchant_repository
+
+# CREATE
+def save(transaction):
+    sql = "INSERT INTO transactions (amount, merchant_id, tag_id) VALUES (%s, %s, %s) RETURNING id"
+    values = [transaction.amount, transaction.merchant.id, transaction.tag.id]
+    results = run_sql(sql, values)
+    id = results[0]["id"]
+    transaction.id = id
+    return transaction
+
+# READ
+def select_all():
+    transactions = []
+    sql = "SELECT * FROM transactions"
+    results = run_sql(sql)
+    for result in results:
+        merchant = merchant_repository.select(result["merchant_id"])
+        tag = tag_repository.select(result["tag_id"])
+        transaction = Transaction(result["amount"], merchant, tag, result["id"])
+        transactions.append(transaction)
+    return transactions
+
+
+def select(id):
+    sql = "SELECT * FROM transactions WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    merchant = merchant_repository.select(result["merchant_id"])
+    tag = tag_repository.select(result["tag_id"])
+    transaction = Transaction(result["amount"], merchant, tag, result["id"])
+    return transaction
+
+
+# UPDATE
+
+def update(transaction):
+    sql = "UPDATE transactions SET (amount, merchant_id, tag_id) = (%s, %s, %s) WHERE id = %s"
+    values = [transaction.amount, transaction.merchant.id, transaction.tag.id, transaction.id]
+    run_sql(sql, values)
+
+
+# DELETE
+
+def delete_all():
+    sql = "DELTETE FROM transactions"
+    run_sql(sql)
+
+
+def delete(id): 
+    sql = "DELETE FROM transactions WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
