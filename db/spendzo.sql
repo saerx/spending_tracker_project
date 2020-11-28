ea00bac87899259e0f0bee177589fd126168a33d
saerx <saerlaith.robyn@gmail.com>
2020-11-28 13:53:47 +0000

    filled in db folder

---
+++ db/spendzo.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS merchants;
+DROP TABLE IF EXISTS tags;
+
+CREATE TABLE merchants (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    merchant_key SERIAL REFERENCES merchants(id),
+    tag_id SERIAL REFERENCES tags(id)
+);
