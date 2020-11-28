b5059574b67b9dd5911655580ffb9c000cbd2c42
saerx <saerlaith.robyn@gmail.com>
2020-11-28 14:04:12 +0000

    tested merchant model

---
+++ run_tests.py
@@ -0,0 +1,7 @@
+import unittest
+ 
+from tests.merchant_test import TestMerchant
+ 
+ 
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/merchant_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from models.merchant import Merchant
+
+class TestMerchant(unittest.TestCase):
+
+    def setUp(self):
+        self.merchant = Merchant("Tesco", 3)
+
+    def test_merchant_has_name(self):
+        self.assertEqual = (self.merchant.name, "Tesco")
+
+    def test_merchant_has_id(self):
+        self.assertEqual = (self.merchant.id, 3)
