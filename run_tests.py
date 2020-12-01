7227b3d7acf57ed5f26cbf845ca49acc0bbef8bd
saerx <saerlaith.robyn@gmail.com>
2020-12-01 16:25:24 +0000

    belatedly tested budget class after realising it wasn't in run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -3,6 +3,7 @@ import unittest
 from tests.merchant_test import TestMerchant
 from tests.tag_test import TestTag
 from tests.transaction_test import TestTransaction
+from tests.budget_test import TestBudget
  
  
 if __name__ == '__main__':
--- tests/budget_test.py
+++ tests/budget_test.py
@@ -3,11 +3,12 @@ import unittest
 from models.budget import Budget
 
 class TestBudget(unittest.TestCase):
+
     def setUp(self):
         self.budget_1 = Budget(600.00)
 
-    def test_budget_has_amount():
+    def test_budget_has_amount(self):
         self.assertEqual(self.budget_1.amount, 600.00)
 
-    def test_budget_id_starts_none():
+    def test_budget_id_starts_none(self):
         self.assertIsNone(self.budget_1.id)
